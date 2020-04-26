d934f4fc9e96f00a57924fb002a8bce2c8d3fee3
Lennart <lnsp@users.noreply.github.com>
2020-04-26 15:12:07 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ async function run() {
 
   try {
     await installer.installValar();
+    
+    core.debug("Submitting build to Valar ...")
 
     // Wait for build ID
     let buildID = '';
@@ -20,9 +22,13 @@ async function run() {
             stderr: (data: Buffer) => {}
         }
     });
+    
+    core.debug("Submitted build with ID " + buildID);
 
     // Follow build logs
     await exec.exec('valar', ['builds', 'logs', '-f', buildID]);
+    
+    core.debug("Build finished.");
   } catch (error) {
     core.setFailed(error.message);
   }
