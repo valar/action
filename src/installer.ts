5876bd9d92ed85a9aae0b8394d1f480bbbaff8c9
Lennart Espe <lnsp@users.noreply.github.com>
2020-04-26 16:44:17 +0200

    Push to version v1.0

--- src/installer.ts
+++ src/installer.ts
@@ -10,15 +10,15 @@ import * as tc from '@actions/tool-cache';
 export async function installValar() {
     let arch = process.platform
     if (arch === 'linux') {
-        let toolPath = tc.find('Valar', '0.5.0');
+        let toolPath = tc.find('Valar', '1.0.0');
 
         if (toolPath) {
             core.debug(`Tool found in cache ${toolPath}`);
         } else {
-            let url = "http://github.com/valar/cli/releases/v0.5.0/download/valar_linux_amd64";
+            let url = "http://github.com/valar/cli/releases/v1.0.0/download/valar_linux_amd64";
             let path = await tc.downloadTool(url);
             await exec.exec('chmod', ['+x', path]);
-            toolPath = await tc.cacheFile(path, 'valar', 'Valar', '0.5.0', arch);
+            toolPath = await tc.cacheFile(path, 'valar', 'Valar', '1.0.0', arch);
         }
         core.addPath(toolPath);
     } else {
